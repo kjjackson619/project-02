6341083bcda191510770cee4968a0fb29637c53b
KJ Jackson <kjjackson619@yahoo.com>
2022-02-10 12:09:47 -0500

    save

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const path = require("path");
 const express = require("express");
-<<<<<<< HEAD
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 const routes = require('./controllers');
-=======
 // const session = require("express-session");
 // const exphbs = require("express-handlebars");
->>>>>>> 5e59bd857931834701a0b14d50b4634b2510e7c1
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,14 +24,11 @@ const sequelize = require("./config/connection");
 //   }),
 // };
 
-<<<<<<< HEAD
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(session(sess));
-=======
 // app.use(session(sess));
->>>>>>> 5e59bd857931834701a0b14d50b4634b2510e7c1
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
