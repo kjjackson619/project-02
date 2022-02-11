410c41ea2ea95131c74fe36d1f5eeb8780bd83e0
AdamAlibraheem <shrineknight000@gmail.com>
2022-02-11 11:26:21 -0500

    added session attributes

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-
-const routes = require('./controllers');
-// const session = require("express-session");
-// const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,18 +19,21 @@ const sequelize = require("./config/connection");
 //   }),
 // };
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(session(sess));
 // app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(require("./models"));
+app.use(require("./controllers/"));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
