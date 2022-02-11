76aa75893a4286d559f127bac51c9ee24e614abc
AdamAlibraheem <shrineknight000@gmail.com>
2022-02-10 21:17:31 -0500

    minor update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(require("./models"));
+app.use(require("./routes"));
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
