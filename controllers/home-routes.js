d96fdce9e6fc0b521381a1460c4457a2db963859
Jon Chastain <chastain.r.jon@gmail.com>
2022-02-14 20:15:47 -0500

    added get route for single category

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,8 @@
+const req = require("express/lib/request");
+const { Item, User } = require("../models");
+const { findAll, getAttributes } = require("../models/Category");
+const sequelize = require('sequelize')
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -17,6 +22,29 @@ router.get("/login", (req, res) => {
 // router.get('/login', (req, res) => {
 //   res.render('login');
 // });
+
+router.get("/:id", (req,res) => {
+  Item.findAll({
+    where: {category_id : req.params.id},
+    attributes: ['id', 'category_id', 'picture', 'item_name'],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  })
+  .then(dbItemData => {
+    const items = dbItemData.map(item => item.get({plain: true}))
+
+    res.render('homepage', {
+      items,
+      loggedIn: req.session.loggedIn
+    })
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).json(err)
+  })
+})
 
 router.get("/signup", (req, res) => {
   res.render("signup");
